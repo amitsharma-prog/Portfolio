244e10ae28f1a0050a6d1be9da1d7cecc99af3f8
amitsharma-prog <amitsharma18543@gmail.com>
2019-07-14 13:37:18 +0530

    ankit hostname semdmail.php

--- sendMail.php
+++ sendMail.php
@@ -8,23 +8,23 @@ use PHPMailer\PHPMailer\Exception;
 require 'autoload.php';
 
 if (sendMailToUser() == true AND sendMailToMe() == true) {
-    echo 'Message send successfull';
+    echo 'Message send successfully';
 } 
 function sendMailToMe() {
-    try {  
+    try {
     //Server settings
     // $mail->SMTPDebug = 2; 
     $mail = new PHPMailer(true);                          // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'amitsharma.galaxeepro.com';  // Specify main and backup SMTP servers
+    $mail->Host = 'ankitvats.galaxeepro.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'contactus@amitsharma.galaxeepro.com';                 // SMTP username
-    $mail->Password = 'amit1sharma';                           // SMTP password
+    $mail->Username = 'ankit@ankitvats.galaxeepro.com';                 // SMTP username
+    $mail->Password = 'Av123456@2019';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom($_POST['email'],$_POST['name']);
+    $mail->setFrom('ankit@ankitvats.galaxeepro.com', 'New Enquiry - ArenaGurgaon');
     $mail->addAddress('amitsharma18543@gmail.com', 'Contact us - ArenaGurgaon');     // Add a recipient
     // $mail->addAddress('aayush.jaiswal984@gmail.com');               // Name is optional
     $mail->addReplyTo($_POST['email'], $_POST['name']);
@@ -37,11 +37,12 @@ function sendMailToMe() {
 
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject =' contact with us.'. $_POST['subject'];
+    $mail->Subject = $_POST['name'] . ' is contact with us.';
     $mail->Body    = '<div style="text-align: left;">
-<h1>C.R. Associates</h1>
+<h1>Arena Gurgaon</h1>
 <p><b>Name</b> :'.$_POST['name'].'</p>
 <p><b>Email</b> :'.$_POST['email'].'</p>
+<p><b>Subject</b> :'.$_POST['subj'].'</p>
 <p><b>Message</b> :'.$_POST['message'].'</p>
 <br/><br/>
 <a href="http://prenits.com"><img src="http://prenits.com/img/logo.png" alt="prenits" style="width: 25%;"></a>
@@ -59,15 +60,15 @@ try {
     //Server settings
     // $mail->SMTPDebug = 2;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'amitsharma.galaxeepro.com';  // Specify main and backup SMTP servers
+    $mail->Host = 'ankitvats.galaxeepro.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'contactus@amitsharma.galaxeepro.com';                 // SMTP username
-    $mail->Password = 'amit1sharma';                           // SMTP password
+    $mail->Username = 'ankit@ankitvats.galaxeepro.com';                 // SMTP username
+    $mail->Password = 'Av123456@2019';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom($_POST['email'],$_POST['name']);
+    $mail->setFrom('ankit@ankitvats.galaxeepro.com', 'Contact Us');
     $mail->addAddress($_POST['email'], $_POST['name']);     // Add a recipient
     // $mail->addAddress('aayush.jaiswal984@gmail.com');               // Name is optional
     $mail->addReplyTo('amitsharma18543@gmail.com', 'Replay');
@@ -81,7 +82,7 @@ try {
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Thanks for contact with us';
-    $mail->Body    = '<h1>C.R. Associates</h1><p>This is a confirmation that we have recived your mail and we will contact to you ASAP.</p>';
+    $mail->Body    = '<h1>Arena Gurgaon</h1><p>This is a confirmation that we have recived your mail and we will contact to you ASAP.</p>';
         // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
         $mail->send();
         return true;
